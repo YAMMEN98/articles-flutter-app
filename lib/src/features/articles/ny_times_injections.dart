4731b0e13c357b7974fd1b1f21052e95aac561ab
Yamen Abd Alrahman <yamen.abd98@gmail.com>
2023-12-03 01:54:59 +0400

    Change Models name

--- lib/src/features/articles/ny_times_injections.dart
+++ lib/src/features/articles/ny_times_injections.dart
@@ -1,8 +1,8 @@
 import 'package:ny_times_app/src/core/network/dio_network.dart';
 import 'package:ny_times_app/src/core/util/injections.dart';
-import 'package:ny_times_app/src/features/articles/data/data_sources/articles_impl_api.dart';
+import 'package:ny_times_app/src/features/articles/data/data_sources/remote/articles_impl_api.dart';
 
-import 'data/data_sources/articles_shared_prefs.dart';
+import 'data/data_sources/local/articles_shared_prefs.dart';
 import 'data/repositories/articles_repo_impl.dart';
 import 'domain/repositories/abstract_articles_repository.dart';
 import 'domain/usecases/articles_usecase.dart';
