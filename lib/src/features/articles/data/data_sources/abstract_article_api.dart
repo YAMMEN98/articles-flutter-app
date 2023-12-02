096bb2df2e4ef51e370e6c8f2cbedb64216ecda5
Yamen Abd Alrahman <yamen.abd98@gmail.com>
2023-12-03 01:53:45 +0400

    Change Models name

--- lib/src/features/articles/data/data_sources/abstract_article_api.dart
+++ lib/src/features/articles/data/data_sources/abstract_article_api.dart
@@ -1,5 +1,5 @@
 import 'package:ny_times_app/src/features/articles/domain/models/articles_params.dart';
-import 'package:ny_times_app/src/features/articles/domain/models/ny_times_article_response_model.dart';
+import 'package:ny_times_app/src/features/articles/domain/models/article_response_model.dart';
 import 'package:ny_times_app/src/features/articles/domain/models/article_model.dart';
 import 'package:ny_times_app/src/features/articles/domain/usecases/articles_usecase.dart';
 
