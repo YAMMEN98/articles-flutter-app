2ea57082b18336272ee7caa86ee534718af15523
Yamen Abd Alrahman <yamen.abd98@gmail.com>
2023-12-03 17:40:46 +0400

    Change article injection file

--- lib/src/core/utils/injections.dart
+++ lib/src/core/utils/injections.dart
@@ -2,7 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:ny_times_app/src/core/network/dio_network.dart';
 import 'package:ny_times_app/src/shared/app_injections.dart';
 import 'package:ny_times_app/src/core/utils/log/app_logger.dart';
-import 'package:ny_times_app/src/features/articles/ny_times_injections.dart';
+import 'package:ny_times_app/src/features/articles/articles_injections.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 
