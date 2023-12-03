2e5a3497748fb8954c2984516b574112bda8b18c
Yamen Abd Alrahman <yamen.abd98@gmail.com>
2023-12-03 17:02:34 +0400

    Article App

--- lib/src/features/articles/presentation/pages/articles_page.dart
+++ lib/src/features/articles/presentation/pages/articles_page.dart
@@ -25,26 +25,291 @@ class ArticlesPage extends StatefulWidget {
 class _ArticlesPageState extends State<ArticlesPage> {
   ArticlesBloc _bloc = ArticlesBloc(articlesUseCase: sl<ArticlesUseCase>());
 
+  // Key for scaffold to open drawer
+  GlobalKey<ScaffoldState> _key = GlobalKey();
+
+  // Refresh controller for list view
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
+  bool isSearching = false;
+
+  // List of articles
+  List<ArticleModel> nyTimesArticles = [];
+
+  // Search text field
+  TextEditingController _searchController = TextEditingController();
+  FocusNode _searchFocusNode = FocusNode();
+
+  // Period
+  int selectedPeriod = 1;
+
+  @override
+  void initState() {
+    // Call event to get ny times article
+    callArticles();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return BackgroundPage(
+      scaffoldKey: _key,
+      withDrawer: true,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          ElevatedButton(
-            onPressed: () {
-              _bloc.add(
-                OnGettingArticlesEvent(
-                  "You",
-                  1,
-                  withLoading: false,
+          // Custom App Bar
+          CustomAppBarWidget(
+            title: isSearching
+                ? TextFieldWidget(
+                    controller: _searchController,
+                    focusNode: _searchFocusNode,
+                    hintText: S.of(context).search,
+                    onChanged: (value) {
+                      _bloc.add(
+                        OnSearchingArticlesEvent(
+                          (value?.trim() ?? ""),
+                        ),
+                      );
+                    },
+                    suffixIcon: IconButton(
+                      padding: EdgeInsets.zero,
+                      constraints: BoxConstraints(),
+                      onPressed: () {
+                        setState(() {
+                          isSearching = !isSearching;
+                          if (isSearching) {
+                            _searchFocusNode.requestFocus();
+                          } else {
+                            _searchFocusNode.unfocus();
+                            _searchController.clear();
+                            callArticles();
+                          }
+                        });
+                      },
+                      icon: Icon(
+                        Icons.close,
+                        size: 20,
+                      ),
+                    ),
+                  )
+                : Text(
+                    S.of(context).ny_times_most_popular,
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+            leading: IconButton(
+              padding: EdgeInsets.zero,
+              constraints: BoxConstraints(),
+              onPressed: () {
+                FocusManager.instance.primaryFocus?.unfocus();
+                _key.currentState!.openDrawer();
+              },
+              icon: Icon(
+                Icons.menu,
+                size: 20,
+              ),
+            ),
+            actions: [
+              // Search
+              if (!isSearching) ...{
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  constraints: BoxConstraints(),
+                  onPressed: () {
+                    setState(() {
+                      isSearching = !isSearching;
+                      if (isSearching) {
+                        _searchFocusNode.requestFocus();
+                      } else {
+                        _searchFocusNode.unfocus();
+                        _searchController.clear();
+                        callArticles();
+                      }
+                    });
+                  },
+                  icon: Icon(
+                    Icons.search,
+                    size: 20,
+                  ),
                 ),
-              );
-            },
-            child: Text("Call Articles"),
+              },
+
+              // Menu
+              PopupMenuButton(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                padding: EdgeInsets.zero,
+                constraints: BoxConstraints(),
+                child: Row(
+                  children: [
+                    Text(
+                      S.of(context).period,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                    SizedBox(
+                      width: 1.sp,
+                    ),
+                    Icon(
+                      Icons.arrow_drop_down,
+                      size: 20,
+                    ),
+                  ],
+                ),
+                onSelected: (value) {
+                  selectedPeriod = int.tryParse(value) ?? 1;
+                  callArticles();
+                },
+                elevation: 3,
+                tooltip: S.of(context).period,
+                itemBuilder: (BuildContext bc) {
+                  return [
+                    PopupMenuItem(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "1",
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          if (selectedPeriod == 1) ...{
+                            Icon(
+                              Icons.check,
+                              color: Theme.of(context).iconTheme.color,
+                              size: 20,
+                            ),
+                          }
+                        ],
+                      ),
+                      value: '1',
+                    ),
+                    PopupMenuItem(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "7",
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          if (selectedPeriod == 7) ...{
+                            Icon(
+                              Icons.check,
+                              color: Theme.of(context).iconTheme.color,
+                              size: 20,
+                            ),
+                          }
+                        ],
+                      ),
+                      value: '7',
+                    ),
+                    PopupMenuItem(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "30",
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          if (selectedPeriod == 30) ...{
+                            Icon(
+                              Icons.check,
+                              color: Theme.of(context).iconTheme.color,
+                              size: 20,
+                            ),
+                          }
+                        ],
+                      ),
+                      value: '30',
+                    )
+                  ];
+                },
+              ),
+
+              ElevatedButton(
+                onPressed: () {
+                  ArticlesBloc bloc =
+                      ArticlesBloc(articlesUseCase: sl<ArticlesUseCase>());
+                  bloc.add(
+                    OnGettingArticlesEvent(
+                      "You",
+                      1,
+                      withLoading: false,
+                    ),
+                  );
+                },
+                child: Text("Call Articles"),
+              ),
+            ],
           ),
+
+          // Space
+          SizedBox(
+            height: Helper.getVerticalSpace(),
+          ),
+
+          // List of articles
+          Expanded(
+            child: BlocConsumer<ArticlesBloc, ArticlesState>(
+              bloc: _bloc,
+              listener: (context, state) {
+                if (state is SuccessGetArticlesState) {
+                  nyTimesArticles.clear();
+                  nyTimesArticles = List.from(state.articles);
+                  _refreshController.refreshCompleted(
+                    resetFooterState: true,
+                  );
+                } else if (state is SearchingState) {
+                  nyTimesArticles.clear();
+                  nyTimesArticles = state.articles;
+                }
+              },
+              builder: (context, state) {
+                if (state is LoadingGetArticlesState) {
+                  return const AppLoader();
+                } else if (state is ErrorGetArticlesState) {
+                  return ReloadWidget.error(
+                    content: state.errorMsg,
+                    onPressed: () {
+                      callArticles();
+                    },
+                  );
+                }
+
+                // Check if there is no data
+                if (nyTimesArticles.isEmpty) {
+                  return ReloadWidget.empty(content: S.of(context).no_data);
+                }
+
+                return SmartRefresher(
+                    enablePullDown: true,
+                    enablePullUp: false,
+                    header: WaterDropHeader(
+                      waterDropColor: Theme.of(context).cardColor,
+                    ),
+                    controller: _refreshController,
+                    onRefresh: _onRefresh,
+                    onLoading: null,
+                    child: ListView.builder(
+                      itemCount: nyTimesArticles.length,
+                      itemBuilder: (context, index) {
+                        return ArticleCardWidget(
+                          nyTimesModel: nyTimesArticles[index],
+                        );
+                      },
+                    ));
+              },
+            ),
+          )
         ],
+      ),
+    );
+  }
+
+  // Call Article Api
+  callArticles({bool withLoading = true}) {
+    _bloc.add(
+      OnGettingArticlesEvent(
+        _searchController.text.trim(),
+        selectedPeriod,
+        withLoading: withLoading,
       ),
     );
   }
