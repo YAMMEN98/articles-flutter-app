1180566c39c2cf93e81085a0f38414426b83383b
Yamen Abd Alrahman <yamen.abd98@gmail.com>
2023-12-10 13:33:06 +0400

    Fix Bug's

--- lib/src/features/articles/articles_injections.dart
+++ lib/src/features/articles/articles_injections.dart
@@ -1,13 +1,15 @@
 import 'package:ny_times_app/src/core/network/dio_network.dart';
 import 'package:ny_times_app/src/core/utils/injections.dart';
+import 'package:ny_times_app/src/features/articles/data/data_sources/remote/abstract_article_api.dart';
 import 'package:ny_times_app/src/features/articles/data/data_sources/remote/articles_impl_api.dart';
+import 'package:ny_times_app/src/features/articles/domain/repositories/abstract_articles_repository.dart';
 import 'data/data_sources/local/articles_shared_prefs.dart';
 import 'data/repositories/articles_repo_impl.dart';
 import 'domain/usecases/articles_usecase.dart';
 
 initArticlesInjections() {
-  sl.registerSingleton(ArticlesImplApi(DioNetwork.appAPI));
-  sl.registerSingleton(ArticlesSharedPrefs(sl()));
-  sl.registerSingleton(ArticlesUseCase(sl()));
-  sl.registerSingleton(ArticlesRepositoryImpl(sl()));
+  sl.registerSingleton<AbstractArticleApi>(ArticlesImplApi(DioNetwork.appAPI));
+  sl.registerSingleton<ArticlesSharedPrefs>(ArticlesSharedPrefs(sl()));
+  sl.registerSingleton<AbstractArticlesRepository>(ArticlesRepositoryImpl(sl()));
+  sl.registerSingleton<ArticlesUseCase>(ArticlesUseCase(sl()));
 }
